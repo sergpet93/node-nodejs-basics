a0bb92aadd3ff102c20e02d965c390cf59732b71
sergpet93 <sergpet_93@mail.ru>
2023-06-16 22:21:42 +0300

    feat: add copy files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'path';
+
+const pathDir = './src/fs/'
+const pathFiles = path.join(pathDir, 'files');
+const pathCopyFiles = path.join(pathDir, 'files-copy');
+const options = {errorOnExist: true, force: false, recursive: true};
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.cp(pathFiles, pathCopyFiles, options);
+    } catch (err) {
+        throw new Error('ErrorFS operation failed');
+    } 
 };
 
 await copy();
