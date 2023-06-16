ea08191e77a404701510ef26a0331681a207cdb6
sergpet93 <sergpet_93@mail.ru>
2023-06-17 00:04:28 +0300

    feat: add read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'path';
+
+const pathDir = './src/fs/files';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const content = await readFile(join(pathDir, 'fileToRead.txt'), 'utf-8');
+        console.log(content);
+    } catch (err) {
+        throw new Error('ErrorFS operation failed');
+    }
 };
 
 await read();
