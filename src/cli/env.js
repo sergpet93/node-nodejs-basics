ce9bf8ff10cdc30761b935c7f72a41253b21f673
sergpet93 <sergpet_93@mail.ru>
2023-06-17 13:13:25 +0300

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const arrayParam = Object.keys(process.env).filter((key) => key.startsWith('RSS_'));
+    const result = arrayParam.map((key) => `${key}=${process.env[key]}`).join('; ');
+    console.log(result);
 };
 
 parseEnv();
