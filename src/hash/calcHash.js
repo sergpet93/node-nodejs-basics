dd1f37087b101a77faa563b3ef7aa3b14f38df55
sergpet93 <sergpet_93@mail.ru>
2023-06-17 18:05:14 +0300

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+const { createHash } = await import("node:crypto");
+import * as fs from 'node:fs/promises';
+
+const pathDir = './src/hash/files/fileToCalculateHashFor.txt'
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const data = await fs.readFile(pathDir);
+        const hash = createHash('sha256');
+        hash.update(data);
+        console.log(hash.digest('hex'));
+    } catch(error) {
+        throw new Error(error);
+    }
 };
 
 await calculateHash();
