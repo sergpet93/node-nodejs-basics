57363ab6fd510395cfaead04f33a4c717bdfe91e
sergpet93 <sergpet_93@mail.ru>
2023-06-17 18:49:21 +0300

    feat: add write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import * as path from 'node:path';
+import {createWriteStream} from 'fs';
+import { fileURLToPath } from 'node:url';
+
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const data = createWriteStream(path.join(pathDir, '/files/fileToWrite.txt'), 'utf8');
+        process.stdout.on('data', (chunk) => {
+            data.write(chunk)
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
 };
 
 await write();
