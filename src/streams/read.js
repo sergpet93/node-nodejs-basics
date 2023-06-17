85753e42acefc3f489f38086e8d0ff0efbc8b655
sergpet93 <sergpet_93@mail.ru>
2023-06-17 18:37:30 +0300

    feat: add read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const readfile = createReadStream(path.join(pathDir, '/files/fileToRead.txt'), 'utf-8');
+        readfile.on('data', chunk => process.stdout.write(chunk + '\n'));
+    } catch (err) {
+        throw new Error(err);
+    }
 };
 
 await read();
